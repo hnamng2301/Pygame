67951b156ff06399fa19b207608c7c6f4200743e
Nam Nguyen <meomaythongminh3@gmail.com>
2022-04-20 11:49:24 +0700

    Collision, random function, change movement of enemy

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import random
+import math
 
 # Initialize pygame
 pygame.init() 
 
 # Create a screen
-screen = pygame.display.set_mode((1520,750), pygame.SCALED) 
+screen = pygame.display.set_mode((1000,600)) 
 
 # Set name of game and icon
 pygame.display.set_caption("Space Invaders")
@@ -14,24 +15,27 @@ pygame.display.set_icon(icon)
 
 # Information for player
 playerImage = pygame.image.load(".\\images\\player.png") # Player image
-playerX = 760  # X root-coordinate of player
-playerY = 650  # Y root-coordinate of player
+playerX = 500  # X root-coordinate of player
+playerY = 480  # Y root-coordinate of player
 playerChange = 0  # Change of player in loop
 
 # Information for enemy
 enemyImage = pygame.image.load(".\\images\\enemy.png") # Enemy image
-enemyX = random.randint(0, 1400)  # X root-coordinate of enemy
-enemyY = random.randint(50, 400)  # Y root-coordinate of enemy
+enemyX = random.randint(0, 1000)  # X root-coordinate of enemy
+enemyY = random.randint(50, 300)  # Y root-coordinate of enemy
 enemyChangeX = 4
-enemyChangeY = 0
+enemyChangeY = 10
 
 # Information for attacking
 bulletImage = pygame.image.load(".\images\\bullet.png")
 bulletX = 0     # X root-coordinate of bullet
-bulletY = 650   # Y root-coordinate of bullet
+bulletY = 480   # Y root-coordinate of bullet
 bulletXChange = 0
 bulletYChange = 10
 bulletState = 'ready'
+
+#Score
+score = 0
 
 # Background
 background = pygame.image.load(".\images\\background.jpg")
@@ -40,16 +44,32 @@ background = pygame.image.load(".\images\\background.jpg")
 def player(x, y): # Function for player change
   screen.blit(playerImage, (x, y))
 
+
 def enemy(x,y): # Function for enemy change
   screen.blit(enemyImage, (x, y))
+
 
 def fire(x, y): # Function for firing
   global bulletState
   bulletState = 'fire'
   screen.blit(bulletImage, (x + 16, y + 10))
-running = True
 
+
+def isCollision(enemyX, enemyY, bulletX, bulletY): # Function for checking collision
+  distance = math.sqrt(math.pow(enemyX - bulletX, 2) + math.pow(enemyY - bulletY, 2))
+  if distance <= 27:
+    return True
+  return False
+
+def randomAppear():
+  x = random.randint(0, 1000)  # X root-coordinate of enemy
+  y = random.randint(50, 300)  # Y root-coordinate of enemy
+  return x, y
+
+##############
+running = True
 while running:
+
   screen.fill((0,0,0)) # RGB : 0 , 0 , 0 -> Black
   screen.blit(background, (0, 0))
   for event in pygame.event.get():
@@ -57,15 +77,15 @@ while running:
       running = False
     
     if event.type == pygame.KEYDOWN:
-    #   print("Key pressed")
       if event.key == pygame.K_LEFT:
-        playerChange -= 3
+        playerChange -= 1
       elif event.key == pygame.K_RIGHT:
-        playerChange += 3
+        playerChange += 1
       elif event.key == pygame.K_SPACE:
-        if bulletState is 'ready':
+        if bulletState == 'ready':
           bulletX = playerX
           fire(bulletX, bulletY)
+
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
         playerChange = 0
@@ -74,23 +94,33 @@ while running:
 
   if playerX <= 0:
     playerX = 0
-  elif playerX >= 1460:
-    playerX = 1460
+  elif playerX >= 940:
+    playerX = 940
     
   enemyX += enemyChangeX
   if enemyX <= 0:
-    enemyChangeX = 4
-  elif enemyX >= 1460:
-    enemyChangeX = -4
-
+    enemyChangeX = 1
+    enemyY += enemyChangeY
+  elif enemyX >= 940:
+    enemyChangeX = -1
+    enemyY += enemyChangeY
+    
   if bulletY <= 0:
-    bulletY = 650
+    bulletY = 480
     bulletState = 'ready'
 
-  if bulletState is 'fire':
+  if bulletState == 'fire':
     fire(playerX, bulletY)
     bulletY -= bulletYChange
     
+  collision = isCollision(enemyX, enemyY, bulletX, bulletY)
+  if collision is True:
+    bulletY = 480
+    bulletState = 'ready'
+    score += 1
+    print(score)
+    enemyX, enemyY = randomAppear()
+
   player(playerX, playerY)
   enemy(enemyX, enemyY)
   pygame.display.update()
